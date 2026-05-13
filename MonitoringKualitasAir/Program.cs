e975b2115ee2a17cbed670ec7fd522f29279c0c8
devitadwiindayani <devitadwiindayani@gmail.com>
2026-05-14 02:36:53 +0700

    tampilan jadi ke login

--- MonitoringKualitasAir/Program.cs
+++ MonitoringKualitasAir/Program.cs
@@ -16,7 +16,7 @@ namespace MonitoringKualitasAir
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            Application.Run(new Login());
         }
     }
 }
