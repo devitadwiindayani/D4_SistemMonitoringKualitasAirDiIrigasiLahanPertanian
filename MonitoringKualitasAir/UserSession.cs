564374be2ee78971fd2e98ab510f9da729e64fb9
devitadwiindayani <devitadwiindayani@gmail.com>
2026-06-26 02:20:28 +0700

    Class User Session

---
+++ MonitoringKualitasAir/UserSession.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MonitoringKualitasAir
+{
+    public static class UserSession
+    {
+        public static string Username { get; set; }
+        public static string Role { get; set; }
+    }
+}
