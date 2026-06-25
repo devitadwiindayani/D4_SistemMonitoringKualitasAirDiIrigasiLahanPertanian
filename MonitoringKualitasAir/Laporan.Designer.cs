4d6f0cbb92078adcade801a20bbf249c9ad46a29
devitadwiindayani <devitadwiindayani@gmail.com>
2026-06-26 02:18:25 +0700

    Form Laporan

--- MonitoringKualitasAir/Laporan.Designer.cs
+++ MonitoringKualitasAir/Laporan.Designer.cs
@@ -28,62 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Laporan));
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.btnTampil = new System.Windows.Forms.Button();
             this.btnCetak = new System.Windows.Forms.Button();
             this.btnKembali = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.cmbTipe = new System.Windows.Forms.ComboBox();
+            this.lblTahunMonitoring = new System.Windows.Forms.Label();
+            this.dtpTahun = new System.Windows.Forms.DateTimePicker();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(29, 23);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(735, 180);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart1.Legends.Add(legend3);
-            this.chart1.Location = new System.Drawing.Point(523, 222);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(45, 56);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.chart1.Series.Add(series3);
-            this.chart1.Size = new System.Drawing.Size(189, 205);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(696, 291);
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
-            // btnTampil
-            // 
-            this.btnTampil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.btnTampil.Location = new System.Drawing.Point(100, 257);
-            this.btnTampil.Name = "btnTampil";
-            this.btnTampil.Size = new System.Drawing.Size(160, 52);
-            this.btnTampil.TabIndex = 2;
-            this.btnTampil.Text = "Tampilkan Data";
-            this.btnTampil.UseVisualStyleBackColor = false;
-            this.btnTampil.Click += new System.EventHandler(this.btnTampil_Click);
-            // 
             // btnCetak
             // 
             this.btnCetak.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.btnCetak.Location = new System.Drawing.Point(308, 257);
+            this.btnCetak.Location = new System.Drawing.Point(610, 375);
             this.btnCetak.Name = "btnCetak";
             this.btnCetak.Size = new System.Drawing.Size(160, 52);
             this.btnCetak.TabIndex = 3;
@@ -94,7 +74,7 @@
             // btnKembali
             // 
             this.btnKembali.BackColor = System.Drawing.Color.White;
-            this.btnKembali.Location = new System.Drawing.Point(199, 354);
+            this.btnKembali.Location = new System.Drawing.Point(29, 375);
             this.btnKembali.Name = "btnKembali";
             this.btnKembali.Size = new System.Drawing.Size(160, 52);
             this.btnKembali.TabIndex = 4;
@@ -102,31 +82,84 @@
             this.btnKembali.UseVisualStyleBackColor = false;
             this.btnKembali.Click += new System.EventHandler(this.button1_Click);
             // 
+            // cmbTipe
+            // 
+            this.cmbTipe.FormattingEnabled = true;
+            this.cmbTipe.Location = new System.Drawing.Point(620, 12);
+            this.cmbTipe.Name = "cmbTipe";
+            this.cmbTipe.Size = new System.Drawing.Size(121, 24);
+            this.cmbTipe.TabIndex = 5;
+            // 
+            // lblTahunMonitoring
+            // 
+            this.lblTahunMonitoring.AutoSize = true;
+            this.lblTahunMonitoring.Location = new System.Drawing.Point(26, 26);
+            this.lblTahunMonitoring.Name = "lblTahunMonitoring";
+            this.lblTahunMonitoring.Size = new System.Drawing.Size(110, 16);
+            this.lblTahunMonitoring.TabIndex = 6;
+            this.lblTahunMonitoring.Text = "Tahun Monitoring";
+            // 
+            // dtpTahun
+            // 
+            this.dtpTahun.Location = new System.Drawing.Point(143, 19);
+            this.dtpTahun.Name = "dtpTahun";
+            this.dtpTahun.Size = new System.Drawing.Size(127, 22);
+            this.dtpTahun.TabIndex = 7;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.White;
+            this.btnLoad.Location = new System.Drawing.Point(293, 18);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(123, 24);
+            this.btnLoad.TabIndex = 8;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(422, 19);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(136, 23);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
             // Laporan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dtpTahun);
+            this.Controls.Add(this.lblTahunMonitoring);
+            this.Controls.Add(this.cmbTipe);
             this.Controls.Add(this.btnKembali);
             this.Controls.Add(this.btnCetak);
-            this.Controls.Add(this.btnTampil);
             this.Controls.Add(this.chart1);
-            this.Controls.Add(this.dataGridView1);
             this.Name = "Laporan";
             this.Text = "Laporan";
             this.Load += new System.EventHandler(this.Laporan_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Button btnTampil;
         private System.Windows.Forms.Button btnCetak;
         private System.Windows.Forms.Button btnKembali;
+        private System.Windows.Forms.ComboBox cmbTipe;
+        private System.Windows.Forms.Label lblTahunMonitoring;
+        private System.Windows.Forms.DateTimePicker dtpTahun;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnReset;
     }
 }
--- MonitoringKualitasAir/Laporan.cs
+++ MonitoringKualitasAir/Laporan.cs
@@ -1,125 +1,212 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Windows.Forms.DataVisualization.Charting;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
-using System.Drawing.Printing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web.Security;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace MonitoringKualitasAir
 {
     public partial class Laporan : Form
     {
-        // Koneksi ke database
-        private readonly SqlConnection conn;
-        private readonly string connectionString =
-                "Data Source=LAPTOP-GO2648H1\\DEVITADWI;Initial Catalog=DBMonitoringKualitasAir;Integrated Security=True";
-        public Laporan()
+        DALMonitoring dbLogic = new DALMonitoring();
+
+        bool isInitializing = true;
+
+        DataTable dt;
+
+        int button = 0;
+
+        private string role;
+
+        // Ubah constructor utamanya agar menerima parameter string roleUser
+        public Laporan(string roleUser)
         {
             InitializeComponent();
-            conn = new SqlConnection(connectionString);
+            role = roleUser;
         }
 
         private void Laporan_Load(object sender, EventArgs e)
         {
-            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dtpTahun.Format = DateTimePickerFormat.Custom;
+            dtpTahun.CustomFormat = "yyyy";
+            dtpTahun.ShowUpDown = true;
+            dtpTahun.MaxDate = DateTime.Now;
+
+            cmbTipe.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            var items = new List<KeyValuePair<string, SeriesChartType>>
+            {
+                new KeyValuePair<string, SeriesChartType>("Kolom", SeriesChartType.Column),
+                new KeyValuePair<string, SeriesChartType>("Pie", SeriesChartType.Pie)
+            };
+
+
+            isInitializing = true;
+
+            cmbTipe.DataSource = items;
+            cmbTipe.DisplayMember = "Key";
+            cmbTipe.ValueMember = "Value";
+            cmbTipe.SelectedIndex = 0;
+
+            isInitializing = false;
+
+            LoadDataChart();
         }
 
-        private void btnTampil_Click(object sender, EventArgs e)
+        public void LoadDataChart()
         {
+            chart1.Series.Clear();
+            chart1.Titles.Clear();
+            chart1.Legends.Clear();
+            chart1.ChartAreas.Clear();
+
+            ChartArea ca = new ChartArea("MainArea");
+
+            ca.AxisX.Title = "Kondisi Air";
+            ca.AxisY.Title = "Jumlah Monitoring";
+            ca.AxisX.LabelStyle.Angle = -45;
+
+            chart1.ChartAreas.Add(ca);
+
             try
             {
-                if (conn.State == ConnectionState.Closed)
-                    conn.Open();
-
-                dataGridView1.Rows.Clear();
-                dataGridView1.Columns.Clear();
-
-                dataGridView1.Columns.Add("tanggal", "Tanggal");
-                dataGridView1.Columns.Add("ph", "pH");
-                dataGridView1.Columns.Add("kondisi", "Kondisi");
-
-                string query = "SELECT tanggal_monitoring, ph_air, kondisi_air FROM MonitoringKualitasAir";
-
-                SqlCommand cmd = new SqlCommand(query, conn);
-                SqlDataReader reader = cmd.ExecuteReader();
-
-                while (reader.Read())
+                if (button == 1)
                 {
-                    dataGridView1.Rows.Add(
-                        Convert.ToDateTime(reader["tanggal_monitoring"]).ToShortDateString(),
-                        reader["ph_air"].ToString(),
-                        reader["kondisi_air"].ToString()
-                    );
+                    dt = dbLogic.GetDataChartByTahun(
+                        dtpTahun.Value);
+                }
+                else
+                {
+                    dt = dbLogic.GetAllDataChart();
                 }
 
-                reader.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Gagal tampil data: " + ex.Message);
-            }
-        }
+                SeriesChartType tipe =
+                    (SeriesChartType)cmbTipe.SelectedValue;
 
-        private void btnCetak_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (conn.State == ConnectionState.Closed)
-                    conn.Open();
-
-                chart1.Series.Clear();
-
-                var s = chart1.Series.Add("pH Air");
-                s.ChartType = SeriesChartType.Column; // biar gampang dibaca
-
-                string query = "SELECT ph_air FROM MonitoringKualitasAir";
-
-                SqlCommand cmd = new SqlCommand(query, conn);
-                SqlDataReader reader = cmd.ExecuteReader();
-
-                int i = 1;
-
-                while (reader.Read())
+                if (tipe == SeriesChartType.Column)
                 {
-                    if (reader["ph_air"] != DBNull.Value)
+                    Series s = new Series("Jumlah Monitoring");
+
+                    s.ChartType = SeriesChartType.Column;
+
+                    foreach (DataRow row in dt.Rows)
                     {
-                        s.Points.AddXY("Data " + i, Convert.ToDouble(reader["ph_air"]));
-                        i++;
+                        string kondisi =
+                            row["KondisiAir"].ToString();
+
+                        int jumlah =
+                            Convert.ToInt32(
+                                row["JumlahMonitoring"]);
+
+                        s.Points.AddXY(
+                            kondisi,
+                            jumlah);
                     }
+
+                    chart1.Series.Add(s);
                 }
+                else
+                {
+                    Series s =
+                        new Series("Jumlah Monitoring");
 
-                reader.Close();
+                    s.ChartType = tipe;
 
-                chart1.Titles.Clear();
-                chart1.Titles.Add("Grafik pH Air");
+                    s.IsValueShownAsLabel = true;
+                    s.Label = "#VALX";
+                    s.LegendText = "#VALX";
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        string kondisi =
+                            row["KondisiAir"].ToString();
+
+                        int jumlah =
+                            Convert.ToInt32(
+                                row["JumlahMonitoring"]);
+
+                        s.Points.AddXY(
+                            kondisi,
+                            jumlah);
+                    }
+
+                    chart1.Series.Add(s);
+                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Gagal tampil chart: " + ex.Message);
+                MessageBox.Show(
+                    "Gagal load data : " +
+                    ex.Message);
             }
+
+            Title title =
+                new Title(
+                    "Jumlah Monitoring Berdasarkan Kondisi Air",
+                    Docking.Top,
+                    new Font("Arial", 12, FontStyle.Bold),
+                    Color.Black);
+
+            chart1.Titles.Add(title);
+
+            Legend legend =
+                new Legend("MainLegend");
+
+            legend.Docking = Docking.Right;
+
+            chart1.Legends.Add(legend);
         }
 
-        private void chart1_Click(object sender, EventArgs e)
+        private void cmbTipe_SelectedIndexChanged(
+            object sender,
+            EventArgs e)
         {
+            if (isInitializing)
+                return;
 
+            LoadDataChart();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void btnLoad_Click(
+            object sender,
+            EventArgs e)
         {
-
+            button = 1;
+            LoadDataChart();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnReset_Click(
+            object sender,
+            EventArgs e)
         {
-            Dashboard f = new Dashboard();
+            button = 0;
+            LoadDataChart();
+        }
+
+        private void btnCetak_Click(
+            object sender,
+            EventArgs e)
+        {
+            MessageBox.Show(
+                "Fitur cetak laporan belum dibuat.");
+        }
+
+        private void button1_Click(
+            object sender,
+            EventArgs e)
+        {
+            Dashboard f = new Dashboard(role); // Ganti sesuai nama form menu utama Anda
             f.Show();
-            this.Hide();
+            this.Close();
+        }
+
+        private void chart1_Click(
+            object sender,
+            EventArgs e)
+        {
+
         }
     }
 }
