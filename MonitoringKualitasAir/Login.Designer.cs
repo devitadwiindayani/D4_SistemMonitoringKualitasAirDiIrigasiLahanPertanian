2dc88870eb05d0c2622c9d040410614e7db6790b
devitadwiindayani <devitadwiindayani@gmail.com>
2026-06-26 02:18:50 +0700

    Form Login

--- MonitoringKualitasAir/Login.Designer.cs
+++ MonitoringKualitasAir/Login.Designer.cs
@@ -28,17 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.btnLogin = new System.Windows.Forms.Button();
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnLogin
             // 
             this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.btnLogin.Location = new System.Drawing.Point(450, 328);
+            this.btnLogin.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnLogin.Location = new System.Drawing.Point(503, 321);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(148, 38);
             this.btnLogin.TabIndex = 0;
@@ -49,14 +52,14 @@
             // txtUsername
             // 
             this.txtUsername.BackColor = System.Drawing.Color.White;
-            this.txtUsername.Location = new System.Drawing.Point(246, 134);
+            this.txtUsername.Location = new System.Drawing.Point(302, 201);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(209, 22);
             this.txtUsername.TabIndex = 1;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(246, 202);
+            this.txtPassword.Location = new System.Drawing.Point(302, 264);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(209, 22);
             this.txtPassword.TabIndex = 2;
@@ -65,7 +68,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(70, 134);
+            this.label1.Location = new System.Drawing.Point(163, 199);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(104, 22);
             this.label1.TabIndex = 3;
@@ -75,18 +78,33 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(70, 208);
+            this.label2.Location = new System.Drawing.Point(169, 264);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(98, 22);
             this.label2.TabIndex = 4;
             this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Gray;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(256, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(298, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "HALAMAN LOGIN";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPassword);
@@ -107,5 +125,6 @@
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- MonitoringKualitasAir/Login.cs
+++ MonitoringKualitasAir/Login.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Windows.Forms.DataVisualization.Charting;
-using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Drawing.Printing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MonitoringKualitasAir
@@ -18,6 +9,7 @@ namespace MonitoringKualitasAir
         private readonly SqlConnection conn;
         private readonly string connectionString =
         "Data Source=LAPTOP-GO2648H1\\DEVITADWI;Initial Catalog=DBMonitoringKualitasAir;Integrated Security=True";
+
         public Login()
         {
             InitializeComponent();
@@ -26,59 +18,73 @@ namespace MonitoringKualitasAir
 
         private void Login_Load(object sender, EventArgs e)
         {
-
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            // Validasi input kosong
-            if (txtUsername.Text == "" || txtPassword.Text == "")
+            // 1. Validasi input jika username atau password kosong
+            if (string.IsNullOrEmpty(txtUsername.Text) || string.IsNullOrEmpty(txtPassword.Text))
             {
-                MessageBox.Show("Username dan Password harus diisi!");
+                MessageBox.Show("Username dan Password harus diisi!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
 
             try
             {
-                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
 
-                // Query cek login
-                SqlCommand cmd = new SqlCommand(
-                    "SELECT role FROM Users WHERE username=@user AND password=@pass", conn);
-
+                // 2. Query untuk mencocokkan username, password, dan mengambil nilai role user
+                SqlCommand cmd = new SqlCommand("SELECT role FROM Users WHERE username=@user AND password=@pass", conn);
                 cmd.Parameters.AddWithValue("@user", txtUsername.Text);
                 cmd.Parameters.AddWithValue("@pass", txtPassword.Text);
 
                 object result = cmd.ExecuteScalar();
 
+                // 3. Jika data user ditemukan di database (Login Sukses)
                 if (result != null)
                 {
                     string role = result.ToString();
 
-                    MessageBox.Show("Login berhasil sebagai " + result.ToString());
+                    // Simpan ke class UserSession (jika Anda menggunakan class session global)
+                    // UserSession.Username = txtUsername.Text;
+                    // UserSession.Role = role;
 
+                    MessageBox.Show("Login berhasil sebagai " + role, "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // 4. Buka Form Dashboard sambil melempar parameter data 'role' agar sinkron dengan form lain
                     Dashboard f = new Dashboard(role);
                     f.Show();
+
+                    // Sembunyikan form login ini
                     this.Hide();
                 }
                 else
                 {
-                    MessageBox.Show("Username atau Password salah!");
+                    // Jika data user tidak ditemukan
+                    MessageBox.Show("Username atau Password salah!", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-
-                conn.Close();
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error: " + ex.Message);
+                MessageBox.Show("Error: " + ex.Message, "Error Database", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // 5. Pastikan koneksi database selalu ditutup kembali
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
             }
         }
 
         private void btnKembali_Click(object sender, EventArgs e)
         {
-            Login f = new Login();
-            f.Show();
-            this.Hide();
+            // Jika tombol kembali ditekan, biasanya menutup aplikasi atau kembali ke halaman awal (jika ada)
+            Application.Exit();
         }
     }
 }
