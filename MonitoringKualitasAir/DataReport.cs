897481b91d7069051a3cf51d4e51607d46888ae0
devitadwiindayani <devitadwiindayani@gmail.com>
2026-06-26 02:16:18 +0700

    Class Data Report

---
+++ MonitoringKualitasAir/DataReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MonitoringKualitasAir // Pastikan namespace SAMA dengan FormRekap
+{
+    public class DataReport
+    {
+        public int IDMonitoring { get; set; }
+        public string NamaLahan { get; set; }
+        public string NamaIrigasi { get; set; }
+        public string NamaPetugas { get; set; }
+        public DateTime TanggalMonitoring { get; set; }
+        public decimal PHAir { get; set; }
+        public string KondisiAir { get; set; }
+        public string Keterangan { get; set; }
+     
+    }
+}
